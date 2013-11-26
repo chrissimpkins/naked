23ffb3ef4b50ad153e3bc723bada1727bfcbc50f
Chris <git.simpkins@gmail.com>
2013-11-26 17:58:46 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
+import os
 from setuptools import setup, find_packages
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), 'docs', fname)).read()
+
 setup(
-    name='naked',
-    version='0.1.0',
-    description='A command line application bootstrap framework',
-    long_description=(open('README.rst').read() + '\n\n' +
-                      open('HISTORY.rst').read() + '\n\n' +
-                      open('AUTHORS.rst').read()),
+    name='Naked',
+    version='0.1.2',
+    description='A command line application framework',
+    long_description=(read('README.rst')),
     url='http://github.com/chrissimpkins/naked/',
     license='MIT',
     author='Christopher Simpkins',
     author_email='chris@zerolabs.net',
     platforms=['any'],
-    py_modules=['naked'],
-    scripts=['naked'],
-    packages=find_packages(exclude=['tests*']),
-    install_requires=['###', '###'],
-    license='MIT License',
+    #py_modules=['naked'],
+    #scripts=['naked'],
+    entry_points = {
+        'console_scripts': [
+            'naked = Naked.app:main'
+        ],
+    },
+    packages=find_packages("lib"),
+    package_dir={'': 'lib'},
+    install_requires=['simplejson', 'PyYAML'],
     keywords='python,command line,CLI,bootstrap,application,app,framework',
     include_package_data=True,
     classifiers=[
