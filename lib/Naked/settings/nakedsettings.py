586fea8919a362e84fe26e5decc020936e30c537
Chris <git.simpkins@gmail.com>
2013-11-27 17:09:49 -0500

    new files

---
+++ lib/Naked/settings/nakedsettings.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# Naked application configuration
+
+if __name__ == '__main__':
+	pass
---
+++ lib/Naked/toolshed/io.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# interface for IO classes, sets the filepath in constructor
+class IO:
+	def __init__(self,filepath):
+		self.filepath = filepath
+
+# file writer class
+class FileWriter(IO):
+	def write(self, text):
+		pass
+
+# file reader class
+class FileReader(IO):
+	def read(self):
+		pass
+
+# read remote git repositories
+class GitReader(IO):
+	pass
+
+if __name__ == '__main__':
+	pass
