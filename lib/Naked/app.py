46f7e07d47b6e411355bff92d14135477a338f97
Chris <git.simpkins@gmail.com>
2013-11-30 13:36:48 -0500

    testing command, option, argument parsing

--- lib/Naked/app.py
+++ lib/Naked/app.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 
-import sys
-import Naked.commandline
-
 def main():
+    import sys
+    import Naked.commandline
 	# TODO: wrap in a try/catch block
     c = Naked.commandline.Command(sys.argv[0], sys.argv[1:])
-    print(c.primarycmd)
-    print(c.option("--help"))
-    print(c.command("new"))
-    #print(command.cmd)
-    #print(command.argv)
-    #print(command.argc)
-    #print(command.arg0)
-    #print(command.arg1)
-    #print(command.arglp)
-    #command.show_args()
+    if c.cmd == "test":
+        if ( c.option("-l") and c.arg("-l") ):
+            print(c.arg("-l"))
+        else:
+            print("Not found")
+        #if c.option("-t"): c.truth = True
+        #print(c.truth)
+    elif c.cmd == "bump":
+        if c.cmd2 == "patch":
+            pass
+        elif c.cmd2 == "minor":
+            pass
+        elif c.cmd2 == "major":
+            pass
+    elif c.help():
+        from Naked.commands.help import Help
+        Help().print_help()
+    elif c.usage():
+        from Naked.commands.usage import Usage
+        Usage().print_usage()
+    elif c.version():
+        from Naked.commands.version import Version
+        Version().print_version()
+    elif c.cmd == "nakedtest":
+        pass
+    else:
+        print("Could not parse the command that you entered.  Please try again.")
 
 if __name__ == '__main__':
 	main()
--- lib/Naked/commandline.py
+++ lib/Naked/commandline.py
@@ -1,51 +1,77 @@
 #!/usr/bin/env python
 
-#############################################################
+#####################################################################
 # Command line command string object
 #   argv = list of command line arguments and options
 #   argc = count of command line arguments and options
 #   arg0 = first argument to command
 #   arg1 = second argument to command
 #   arglp = last positional argument to command
-#   argprimarycmd = primary command for command suite application
-#   argsecondarycmd = secondary command for command suite application
-##############################################################
+#   cmd = primary command for command suite application
+#   cmd2 = secondary command for command suite application
+#####################################################################
 class Command:
-	def __init__(self, cmdarg, argv):
-		# from Naked.commandline import Argument # import the Argument class
-		# from Naked.commandline import Option # import Option class
-		self.argobj = Argument(argv) # create a Argument obj
+	def __init__(self, app_path, argv):
+		self.argobj = Argument(argv) # create an Argument obj
 		self.optobj = Option(argv) # create an Option obj
-		self.cmd = cmdarg   # path to command executable file
+		self.app = app_path   # path to application executable file
 		self.argv = argv    # list of the command arguments argv[0] is first argument
 		self.argc = len(argv)  # length of the argument list
-		self.arg0 = self.argobj._getArg(0) # define the first argument as a local variable
-		self.arg1 = self.argobj._getArg(1) # define the second argument as a local variable
+		self.arg0 = self.argobj._getArg(0) # define the first positional argument as a local variable
+		self.arg1 = self.argobj._getArg(1) # define the second positional argument as a local variable
 		self.arglp = self.argobj._getArg(len(argv) - 1) # define the last positional argument as a local variable
-		self.primarycmd = self.arg0  # define the primary command variable as the first positional argument (user dependent & optional, may be something else)
-		self.secondarycmd = self.arg1 # define the secondary command variable as the second positional argument (user dependent & optional, may be something else)
+		self.cmd = self.arg0  # define the primary command variable as the first positional argument (user dependent & optional, may be something else)
+		self.cmd2 = self.arg1 # define the secondary command variable as the second positional argument (user dependent & optional, may be something else)
 		# self.options
 
+	# test that the command includes an option (option_string parameter)
 	def option(self, option_string, argument_required = False):
 		if (option_string in self.optobj):
-			## TODO: add handling for options that require an argument
 			return True
 		else:
 			return False
 
+	# test that the command includes a primary command suite command (cmd_str parameter)
 	def command(self, cmd_str, argument_required = False):
-		if (cmd_str == self.primarycmd):
+		if (cmd_str == self.cmd):
 			## TODO: add handling for command that requires additional arguments
 			return True
 		else:
 			return False
 
-	# print the arguments with their corresponding agv list position to std out
+	# return the next positional argument to a command line object (e.g. an option that requires an argument)
+	def arg(self, arg_recipient):
+		recipient_position = self.argobj._getArgPosition(arg_recipient)
+		return self.argobj._getArgNext(recipient_position)
+
+	# did user request help?
+	def help(self):
+		if ( (self.option("--help")) or (self.option("-h")) or (self.cmd == "help") ):
+			return True
+		else:
+			return False
+
+	# did user request usage info?
+	def usage(self):
+		if ( (self.argc == 0) or (self.option("--usage")) or (self.cmd == "usage") ):
+			return True
+		else:
+			return False
+
+	# did user request version info?
+	def version(self):
+		if ( (self.option("--version")) or (self.option("-v")) or (self.cmd == "version") ):
+			return True
+		else:
+			return False
+
+	# print the arguments with their corresponding argv list position to std out
 	def show_args(self):
 		x = 0
 		for arg in self.argv:
 			print("argv[" + str(x) + "] = " + arg)
 			x = x + 1
+
 
 # Command line argument object (list object inherited from Python list)
 class Argument(list):
@@ -58,6 +84,21 @@ class Argument(list):
 	def _getArg(self, position):
 		if ( self.argv ) and ( len(self.argv) > position ):
 			return self.argv[position]
+		else:
+			return ""
+
+	# return position of user specified argument in the argument list
+	def _getArgPosition(self, test_arg):
+		if ( self.argv ):
+			if test_arg in self.argv:
+				return self.argv.index(test_arg)
+			else:
+				return -1
+
+	# return the argument at the next position following a user specified positional argument (e.g. for argument to an option in cmd)
+	def _getArgNext(self, position):
+		if len(self.argv) > (position + 1):
+			return self.argv[position + 1]
 		else:
 			return ""
 
