7790ced334a34300737c88fa8164ed269e2b8c29
Chris <git.simpkins@gmail.com>
2013-11-30 13:38:52 -0500

    settings.py added

---
+++ lib/Naked/settings.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+## Application name
+app_name = "naked"
+
+## Current version number
+version = "0.1.7"
+
+## Usage string for help documentation
+usage = """command [option(s)] [argument(s)]
+--- Use 'naked help' for detailed help ---
+"""
+
+## Help documentation
+help = """
+-------------------------------------
+Naked
+ A command line application framework
+ Copyright 2013 Christopher Simpkins
+ MIT license
+--------------------------------------
+
+USAGE
+  naked
+
+COMMANDS
+
+OPTIONS
+
+EXAMPLES
+
+NOTES
+
+SOURCE REPOSITORY
+
+ISSUE REPORTING
+
+"""
+
