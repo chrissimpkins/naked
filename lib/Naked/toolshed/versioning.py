059b729833f5f602935ba933ec95f5519f2127b7
Chris <git.simpkins@gmail.com>
2013-12-23 16:40:01 -0500

    updates to versioning library file

--- lib/Naked/toolshed/versioning.py
+++ lib/Naked/toolshed/versioning.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
 
-from Naked.settings import major_version as major_version
-from Naked.settings import minor_version as minor_version
-from Naked.settings import patch_version as patch_version
+#TODO : add to tests
 
 class VersionEditor:
-	def __init__(self):
-		self.app_version = major_version + "." + minor_version + "." + patch_version
+	def __init__(self, major_version, minor_version, patch_version):
+		self.major_version = major_version
+		self.minor_version = minor_version
+		self.patch_version = patch_version
 
-	def get_version(self):
-		return self.app_version
+	# increment patch version by 1
+	def increment_version_patch(self):
+		patch_int = int(self.patch_version) # convert to int
+		patch_int += 1
+		return str(patch_int) # return as a string
 
-	def bump_version_patch(self):
-		pass
+	# increment the minor version by 1
+	def increment_version_minor(self):
+		minor_int = int(self.minor_version)
+		minor_int += 1
+		return str(minor_int)
 
-	def bump_version_minor(self):
-		pass
+	# increment the major version by 1
+	def increment_version_major(self):
+		major_int = int(self.major_version)
+		major_int += 1
+		return str(major_int)
 
-	def bump_version_major(self):
-		pass
+if __name__ == '__main__':
+	pass
