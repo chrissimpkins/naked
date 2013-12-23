fd4d28fcede0cfff69930309f5e70cc313b2f3dd
Chris <git.simpkins@gmail.com>
2013-12-23 16:39:18 -0500

    updates to version command file

--- lib/Naked/commands/version.py
+++ lib/Naked/commands/version.py
@@ -8,10 +8,14 @@ class Version:
 		self.minor_version = Naked.settings.minor_version
 		self.patch_version = Naked.settings.patch_version
 		self.name = Naked.settings.app_name
+		self.app_version_string = self.name + " " + self.major_version + "." + self.minor_version + "." + self.patch_version
+		self.version_string = self.major_version + "." + self.minor_version + "." + self.patch_version
 
 	def print_version(self):
-		version_string = self.name + " " + self.major_version + "." + self.minor_version + "." + self.patch_version
-		print(version_string)
+		print(self.app_version_string)
+
+	def get_version(self):
+		return self.version_string
 
 
 if __name__ == '__main__':
