e9cf358c288c346e4d6ad50d3d9dbb0da798ff3c
Chris <git.simpkins@gmail.com>
2013-12-31 02:49:47 -0500

    new tests added to test_IO.py

--- tests/test_IO.py
+++ tests/test_IO.py
@@ -5,7 +5,7 @@ import unittest
 import os
 from Naked.toolshed.file import FileReader
 from Naked.toolshed.file import FileWriter
-from Naked.toolshed.ospaths import PathMaker
+from Naked.toolshed.system import make_path
 
 
 class NakedIOReadWriteTest(unittest.TestCase):
@@ -15,16 +15,19 @@ class NakedIOReadWriteTest(unittest.TestCase):
  & more string"""
 		self.unicode_string = u"Hey! It's Bengali ব য,\nand here is some more ২"
 		self.ascii_string = "Check it! This is ascii text!"
-		self.pm = PathMaker()
-		self.ascii_path = self.pm.make_os_independent_path("testfiles", "ascii.txt")
-		self.unicode_path = self.pm.make_os_independent_path("testfiles", "unicode.txt")
-		self.bogus_path = self.pm.make_os_independent_path("testfiles", "bogus.txt")
+		self.latin_string = "grégory"
+		self.ascii_path = make_path("testfiles", "ascii.txt")
+		self.unicode_path = make_path("testfiles", "unicode.txt")
+		self.unicode2_path = make_path("testfiles", "unicode2.txt")
+		self.utfsixteen_path = make_path("testfiles", "sixteen.txt")
+		self.latin_path = make_path("testfiles", "latin.txt")
+		self.bogus_path = make_path("testfiles", "bogus.txt")
 
 	def tearDown(self):
 		pass
 
     #------------------------------------------------------------------------------
-    # ASCII file tests
+    # ASCII read & write file tests
     #------------------------------------------------------------------------------
 	def test_file_ascii_readwrite(self):
 		"""Test write and read of ascii encoded file"""
@@ -36,6 +39,17 @@ class NakedIOReadWriteTest(unittest.TestCase):
 		"""Test that missing ascii file throws IOError"""
 		with (self.assertRaises(IOError)):
 			FileReader(self.bogus_path).read()
+
+	def test_file_ascii_readwrite_append(self):
+		"""Test append of ascii text to existing file"""
+		FileWriter(self.ascii_path).append(self.ascii_string) #append a second string of the ascii text
+		ascii_text = FileReader(self.ascii_path).read()
+		self.assertEqual(ascii_text, (self.ascii_string)*2) #confirm that it equals two of the ascii strings
+
+	def test_file_ascii_readwrite_append_missingfile(self):
+		"""Test append of ascii text raises IOError when file missing"""
+		with (self.assertRaises(IOError)):
+			FileWriter(self.bogus_path).append(self.ascii_string)
 
 	def test_file_ascii_safewrite(self):
 		"""Test safe_write() to confirm does not overwrite existing file"""
@@ -63,11 +77,54 @@ class NakedIOReadWriteTest(unittest.TestCase):
 		unicode_text = FileReader(self.unicode_path).read_utf8()
 		self.assertEqual(unicode_text, self.unicode_string)
 
+	def test_file_utf8_readas_writeas(self):
+		"""Test read_as & write_as with utf-8 encoding"""
+		FileWriter(self.unicode2_path).write_as(self.unicode_string, "utf-8")
+		unicode_text = FileReader(self.unicode2_path).read_as("utf-8")
+		self.assertEqual(unicode_text, self.unicode_string)
+
+	def test_file_utf8_readwrite_append(self):
+		FileWriter(self.unicode_path).append_utf8(self.unicode_string)
+		unicode_text = FileReader(self.unicode_path).read_utf8()
+		self.assertEqual(unicode_text, (self.unicode_string*2))
+
+	def test_file_utf8_write_raises_unicodeerror(self):
+		"""Test write of a utf-8 encoded file with write method raises UnicodeEncodeError"""
+		with (self.assertRaises(UnicodeEncodeError)):
+			FileWriter(self.unicode_path).write(self.unicode_string)
+
+	def test_file_utf8_readwrite_raises_unicodeerror(self):
+		"""Test read of utf-8 encoded file with read method & attempt to encode as utf-8 raises exception"""
+		with (self.assertRaises(UnicodeDecodeError)):
+			FileWriter(self.unicode_path).write_utf8(self.unicode_string)
+			unicode_text = FileReader(self.unicode_path).read()
+			encoded_text = unicode_text.encode("utf-8")
+
 	def test_file_utf8_readwrite_missing_file(self):
 		"""Test that missing utf-8 file throws IOError"""
 		with (self.assertRaises(IOError)):
 			FileReader(self.bogus_path).read_utf8()
 
+	#------------------------------------------------------------------------------
+	# FILE COMPRESSION tests
+	#------------------------------------------------------------------------------
+	def test_file_gzip_ascii_readwrite(self):
+		"""Test gzip compression and read from compressed ascii text file"""
+		FileWriter(self.ascii_path).gzip(self.ascii_string)
+		gzip_contents = FileReader(self.ascii_path + ".gz").read_gzip()
+		self.assertEqual(gzip_contents, self.ascii_string)
+
+	def test_file_gzip_utf8_readwrite(self):
+		"""Test gzip compression and read from compressed unicode text file without explicit utf-8 decode"""
+		FileWriter(self.unicode_path).gzip(self.unicode_string)
+		gzip_contents = FileReader(self.unicode_path + ".gz").read_gzip() # when read without explicit utf-8 decoding, the strings will not match
+		self.assertNotEqual(gzip_contents, self.unicode_string)
+
+	def test_file_gzip_utf8_readwrite_explicit_decode(self):
+		"""Test gzip compression and read from compressed unicode text file with explicit utf-8 decode"""
+		FileWriter(self.unicode_path).gzip(self.unicode_string)
+		gzip_contents = FileReader(self.unicode_path + ".gz").read_gzip("utf-8") # when read with explicit utf-8 decoding, strings should match
+		self.assertEqual(gzip_contents, self.unicode_string)
 
 
 
