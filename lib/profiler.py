da764d8f091f53091287ba4e98b9728182867183
Chris <git.simpkins@gmail.com>
2014-01-03 01:46:03 -0500

    new profiler module

---
+++ lib/profiler.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import cProfile, pstats, StringIO
+
+def profile():
+	#------------------------------------------------------------------------------
+	# Setup a profile
+	#------------------------------------------------------------------------------
+	pr = cProfile.Profile()
+	#------------------------------------------------------------------------------
+	# Enter setup code below
+	#------------------------------------------------------------------------------
+		# Optional: include setup code below (e.g. imports, instantiation, variable assignments, etc)
+
+
+	#------------------------------------------------------------------------------
+	# Start profiler
+	#------------------------------------------------------------------------------
+	pr.enable()
+
+	#------------------------------------------------------------------------------
+	# Enter code to be profiled below
+	#------------------------------------------------------------------------------
+		# include profiled code here
+
+
+
+	#------------------------------------------------------------------------------
+	# End profiler & print results to std out - do not modify below
+	#------------------------------------------------------------------------------
+	pr.disable()
+	s = StringIO.StringIO()
+	sortby = 'cumulative'
+	ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+	ps.print_stats()
+	print s.getvalue()
+
+if __name__ == '__main__':
+	profile()
