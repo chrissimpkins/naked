c1aaf04765bea1b9acb88a5f20078d52b46a4b48
Chris <git.simpkins@gmail.com>
2014-01-09 00:54:37 -0500

    updated help

--- lib/Naked/settings.py
+++ lib/Naked/settings.py
@@ -10,7 +10,7 @@ app_name = "naked"
 #------------------------------------------------------------------------------
 major_version = "0"
 minor_version = "1"
-patch_version = "10"
+patch_version = "11"
 
 #------------------------------------------------------------------------------
 # Debug Flag (switch to False for production release code)
@@ -28,20 +28,27 @@ usage = """<primary command> [secondary command] [option(s)] [argument(s)]
 # Help String
 #------------------------------------------------------------------------------
 help = """
--------------------------------------
-Naked
- A command line application framework
- Copyright 2013 Christopher Simpkins
- MIT license
---------------------------------------
+---------------------------------------------------
+Naked | A Python command line application framework
+Copyright 2014 Christopher Simpkins
+MIT license
+---------------------------------------------------
 
 USAGE
 
   naked <primary command> [secondary command] [option(s)] [argument(s)]
 
-The <primary command> is mandatory and includes one of the commands in the following section.  The [bracketed] usage structure is optional and dependent upon the primary command that you use.  Use the command 'naked <primary command> help' for more information about a specific primary command.
+The <primary command> is mandatory and includes one of the commands in the following section.  The [bracketed] syntax structure is optional and dependent upon the primary command that you use.  Use the command 'naked <primary command> help' for more information about a specific primary command.
 
-PRIMARY COMMANDS
+PRIMARY COMMANDS [secondary commands]
+
+  bump 		[major, minor, patch]
+  help 		- none -
+  locate	[uniset, settings]
+  make 		[project, uniset]
+  push 		- none -
+  usage 	- none -
+  version 	- none -
 
 OPTIONS
 
@@ -49,9 +56,7 @@ Options are dependent upon the primary command that you use.  You can enter:
 
   naked <primary command> help
 
-for more information about available options for a primary command.
-
-COMMONLY USED COMMANDS
+for more information about available options for a specific command.
 
 SOURCE REPOSITORY
 
