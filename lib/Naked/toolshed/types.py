10ee2483b35eacb9f5c941bf8a3778d52e8c83ca
Chris <git.simpkins@gmail.com>
2014-01-15 07:29:18 -0500

    multiple new methods, new types

--- lib/Naked/toolshed/types.py
+++ lib/Naked/toolshed/types.py
@@ -44,6 +44,13 @@ class NakedObject:
     def setAttribute(self, attribute, value):
         setattr(self, attribute, value)
 
+    #------------------------------------------------------------------------------
+    # [ hasAttribute method ] (boolean)
+    #  returns truth test for presence of an `attribute` name on the NakedObject
+    #------------------------------------------------------------------------------
+    def hasAttribute(self, attribute):
+        return hasattr(self, attribute)
+
 #------------------------------------------------------------------------------
 # [ XDict class ]
 #   An inherited extension to the dictionary object that permits attachment of attributes
@@ -59,15 +66,56 @@ class XDict(dict, NakedObject):
 
     #------------------------------------------------------------------------------
     #  +- overload extends XDict with another dictionary
-    #  overwrites existing keys with another_dict keys if they are the same
+    #  overwrites existing keys with another_dict (right sided argument) keys if they are the same
+    #  returns the updated XDict object
     #------------------------------------------------------------------------------
     def __iadd__(self, another_dict):
         self.update(another_dict)
         return self
 
     #------------------------------------------------------------------------------
+    # << overload extends XDict with another dictionary
+    #  overwrites existing keys with another_dict (right sided argument) keys if they are the same
+    #  modifies the XDict in place, no return value
+    #------------------------------------------------------------------------------
+    def __lshift__(self, another_dict):
+        self.update(another_dict)
+
+
+    #------------------------------------------------------------------------------
+    # Value Methods
+    #------------------------------------------------------------------------------
+    # return XTuple of minimum value
+    def min_val(self):
+        result = min(zip(self.values(), self.keys()))
+        return XTuple( result, {'val': result[0], 'key': result[1]})
+
+    # return XTuple of maximum value
+    def max_val(self):
+        result = max(zip(self.values(), self.keys()))
+        return XTuple( result, {'val': result[0], 'key': result[1]} )
+
+    # sum values
+    def sum_vals(self):
+        return sum(self.values())
+
+    # map a function to every value in the dictionary
+    def map_to_vals(self, the_func):
+        return XDict( zip(self, map(the_func, self.values())), self._getAttributeDict())
+
+    #------------------------------------------------------------------------------
+    # Comparison Methods
+    #------------------------------------------------------------------------------
+    def intersection(self, another_dict):
+        return self.keys() & another_dict.keys()
+
+    def difference(self, another_dict):
+        return self.keys() - another_dict.keys()
+
+
+    #------------------------------------------------------------------------------
     # [ iter method ] (tuple of each key and value in dictionary)
-    #   Generator method that returns a tuples of key, value in dictionary
+    #   Generator method that returns tuples of key, value in dictionary
     #   uses appropriate method from Python 2 and 3
     #------------------------------------------------------------------------------
     def iter(self):
@@ -140,6 +188,23 @@ class XList(list, NakedObject):
     def surround(self, before, after):
         return [ "".join([before, x, after]) for x in self ]
 
+    # Numeric methods
+    def max(self):
+        return max(self)
+
+    def min(self):
+        return min(self)
+
+    def sum(self):
+        return sum(self)
+
+    # remove duplicate items in an XList
+    def remove_duplicates(self):
+        return XList( set(self), self._getAttributeDict() )
+
+    # map a function to every item in the XList
+    def map_to_items(self, the_func):
+        return XList( map(the_func, self), self._getAttributeDict())
 
     #------------------------------------------------------------------------------
     # Conversion Methods
@@ -175,6 +240,39 @@ class XList(list, NakedObject):
         from itertools import chain
         return chain(*lists)
 
+
+#------------------------------------------------------------------------------
+# [ XPriorityQueue class ]
+#
+#------------------------------------------------------------------------------
+import heapq
+class XPriorityQueue(NakedObject):
+    def __init__(self, initial_iterable=[], attributes={}):
+        NakedObject.__init__(self, attributes)
+        self._queue = []
+        self._index = 0
+
+    # O(log n) complexity
+    def push(self, the_object, priority):
+        heapq.heappush(self._queue, (-priority, self._index, the_object))
+        self._index += 1
+
+    # O(log n) complexity
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+
+#------------------------------------------------------------------------------
+# [ XQueue class ]
+#
+#------------------------------------------------------------------------------
+from collections import deque
+class XQueue(deque, NakedObject):
+    def __init__(self, initial_iterable=[], attributes={}, max_length=10):
+        deque.__init__(self, initial_iterable, max_length)
+        NakedObject.__init__(self, attributes)
+
+
 #------------------------------------------------------------------------------
 # [ XSet class ]
 #  An inherited extension to the mutable set object that permits attribute assignment
@@ -184,6 +282,16 @@ class XSet(set, NakedObject):
     def __init__(self, set_obj, attributes={}):
         set.__init__(self, set_obj)
         NakedObject.__init__(self, attributes)
+
+    #   << operator is overloaded to extend the XSet with a second set
+    def __lshift__(self, another_set):
+        self.update(another_set)
+        return self
+
+    #   += operator overload to extend the XSet with a second set
+    def __iadd__(self, another_set):
+        self.update(another_set)
+        return self
 
     def xlist(self):
         attr_dict = self._getAttributeDict()
@@ -240,18 +348,56 @@ class XString(str):
     def getAttribute(self, attribute):
         return getattr(self, attribute)
 
+    # fastest substring search truth test
+    def contains(self, substring):
+        return substring in self
+
+#------------------------------------------------------------------------------
+# [ XTuple class ]
+#
+#------------------------------------------------------------------------------
+class XTuple(tuple):
+    def __new__(cls, the_tuple, attributes={}):
+        tup_obj = tuple.__new__(cls, the_tuple)
+        if len(attributes) > 0:
+            for key in attributes:
+                setattr(tup_obj, key, attributes[key])
+        from sys import version_info
+        tup_obj.py_version = (version_info[0], version_info[1], version_info[2])
+        return tup_obj
+
+
+
 
 if __name__ == '__main__':
+    pass
     # nl = XList(['a', 'b', 'c'], {"version":"1.0.1", "test":"code"})
     # nl << ['d', 'e', 'f']
     # the_list = list(range(5000))
     # nl = XList(the_list)
-    # nl.is_in('4999')
+    # nq = XPriorityQueue()
+    # nq.push('test', 5)
+    # nq.push('one', 3)
+    # nq.push('another', 4)
+    # print(nq.pop())
+    # print(nq.pop())
+    # print(nq.pop())
 
-    # xs = XFSet({'test', 'true', 'false'}, {'bonus': 'candy', 'test': 'another'})
-    # print(xs.xlist())
+    # nl = XList(['test', 'test', 'another'], {'p': 'attribute'})
+    # print(nl)
+    # nl = nl.remove_dupes()
+    # print(nl)
+    # nq = XQueue(nl, max_length=2)
+    # print(nq)
+
+    # xs = XSet({'test', 'true', 'false'}, {'bonus': 'candy', 'test': 'another'})
+    # xs += {'bogus', 'yep'}
+    # print(xs)
 
     # xd = XDict({'test': 'testing', 'another': 'yep'}, {'a': '1', 'b': '2'})
-    # ad = {'this': 'yes', 'is':'more'}
-    # xd += ad
+    # ad = {'test': 'yes', 'is':'more'}
+    # xd << ad
     # print(xd)
+
+    # xstr = XString("This is a really long string that contains a few cool things.")
+    # xstr.contains('few')
