3a2a763b5a0b37cf461ff5beafb5662db483de29
Chris <git.simpkins@gmail.com>
2014-01-20 07:36:05 -0500

    new methods on Naked types

--- lib/Naked/toolshed/types.py
+++ lib/Naked/toolshed/types.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
 ##TODO : try/catch around the getter methods in case they are missing
+from Naked.settings import debug as DEBUG_FLAG
 
 #------------------------------------------------------------------------------
 # [ NakedObject class ]
@@ -56,8 +58,18 @@ class XDict(dict, NakedObject):
     #------------------------------------------------------------------------------
 
     #------------------------------------------------------------------------------
+    # + overload extends XDict with one or more other dictionaries
+    #   overwrites existing keys with key:value pairs from new dictionaries if they are the same keys
+    #   returns the updated XDict object
+    #------------------------------------------------------------------------------
+    def __add__(self, *other_dicts):
+        for the_dict in other_dicts:
+            self.update(the_dict)
+        return self
+
+    #------------------------------------------------------------------------------
     #  +- overload extends XDict with another dictionary
-    #  overwrites existing keys with another_dict (right sided argument) keys if they are the same
+    #  overwrites existing keys with another_dict (right sided argument) keys if they are the same keys
     #  returns the updated XDict object
     #------------------------------------------------------------------------------
     def __iadd__(self, another_dict):
@@ -67,20 +79,20 @@ class XDict(dict, NakedObject):
     #------------------------------------------------------------------------------
     # << overload extends XDict with another dictionary
     #  overwrites existing keys with another_dict (right sided argument) keys if they are the same
-    #  modifies the XDict in place, no return value
     #------------------------------------------------------------------------------
     def __lshift__(self, another_dict):
         self.update(another_dict)
+        return self
 
     #------------------------------------------------------------------------------
     # XDict Value Methods
     #------------------------------------------------------------------------------
-    # return XTuple of minimum value
+    # return XTuple of minimum value, key
     def min_val(self):
         result = min(zip(self.values(), self.keys()))
-        return XTuple( result, {'val': result[0], 'key': result[1]})
+        return XTuple( result, {'val': result[0], 'key': result[1]} )
 
-    # return XTuple of maximum value
+    # return XTuple of maximum value, key
     def max_val(self):
         result = max(zip(self.values(), self.keys()))
         return XTuple( result, {'val': result[0], 'key': result[1]} )
@@ -89,16 +101,34 @@ class XDict(dict, NakedObject):
     def sum_vals(self):
         return sum(self.values())
 
-    from Naked.toolshed.benchmarking import timer_trials_benchmark
-    @timer_trials_benchmark
+    # case sensitive value counts
     def value_count(self, value_name):
-        from collections import Counter
-        c = Counter(self.values())
-        return c[value_name]
+        count = 0
+        for test_string in self.values():
+            if value_name == test_string:
+                count += 1
+        return count
+
+    # case insensitive string value counts
+    def value_count_ci(self, value_name):
+        value_name = value_name.lower()
+        lower_case_vals = [ x.lower() for x in self.values() ]
+        count = 0
+        for test_string in lower_case_vals:
+            if value_name in test_string:
+                count += 1
+        return count
 
     # map a function to every value in the dictionary
     def map_to_vals(self, the_func):
-        return XDict( zip(self, map(the_func, self.values())), self._getAttributeDict())
+        return XDict( zip(self, map(the_func, self.values())), self._getAttributeDict() )
+
+    # map a function to dicitionary values conditional upon result of a function on keys
+    def conditional_map_to_vals(self, conditional_function, result_function):
+        for key, value in self.xitems():
+            if conditional_function(key):
+                self[key] = result_function(self[key])
+        return self
 
     #------------------------------------------------------------------------------
     # XDict Key Methods
@@ -108,6 +138,31 @@ class XDict(dict, NakedObject):
 
     def difference(self, another_dict):
         return self.keys() - another_dict.keys()
+
+    # union of one or more dictionaries, key: value overwrite for duplicate keys
+    def union(self, *other_dicts):
+        for new_dict in other_dicts:
+            self.update(new_dict)
+        return self
+
+    # map a function to every key in the dictionary
+    def map_to_keys(self, the_func):
+        return XDict( zip(map(the_func, self.keys()), self.values()), self._getAttributeDict())
+
+    #return random {key: value}
+    def random(self):
+        import random
+        random_key = random.choice(self.keys())
+        return {random_key: self[random_key]}
+
+    # return a random sample of `number_of_items` {key:value} pairs as a new dict
+    def random_sample(self, number_of_items):
+        import random
+        random_key_list = random.sample(self, number_of_items)
+        new_dict = {}
+        for item in random_key_list:
+            new_dict[item] = self[item]
+        return new_dict
 
     #------------------------------------------------------------------------------
     # [ xitems method ] (tuple of each key and value in dictionary)
@@ -133,6 +188,12 @@ class XList(list, NakedObject):
     #------------------------------------------------------------------------------
     # Operator Overloads/Defs
     #------------------------------------------------------------------------------
+
+    # + operator overload extends the XList with one or more lists
+    def __add__(self, *other_lists):
+        for the_list in other_lists:
+            self.extend(the_list)
+        return self
 
     #   += operator overload to extend the XList with the argument list
     def __iadd__(self, another_list):
@@ -201,7 +262,62 @@ class XList(list, NakedObject):
 
     # map a function to every item in the XList
     def map_to_items(self, the_func):
-        return XList( map(the_func, self), self._getAttributeDict())
+        return XList( map(the_func, self), self._getAttributeDict() )
+
+    # map a function to items that return True on `conditional_function`
+    def conditional_map_to_items(self, conditional_function, result_function):
+        for index, item in enumerate(self):
+            if conditional_function(item):
+                self[index] = result_function(item)
+        return self
+
+    # count of an item of `test_string`
+    def count_item(self, test_string):
+        count = 0
+        for item_value in self:
+            if test_string == item_value:
+                count += 1
+        return count
+
+    # return a random item from list
+    def random(self):
+        import random
+        return random.choice(self)
+
+    # return random sample of `number_of_items` items from list
+    def random_sample(self, number_of_items):
+        import random
+        return random.sample(self, number_of_items)
+
+    # randomly shuffle the items in the list
+    def shuffle(self):
+        import random
+        random.shuffle(self)
+        return self
+
+    # single wildcard match
+    def wildcard_match(self, wildcard):
+        if self.hasAttribute('nkd_fnmatchcase'):
+            fnmatchcase = self.nkd_fnmatchcase
+        else:
+            from fnmatch import fnmatchcase
+            self.nkd_fnmatchcase = fnmatchcase
+        return [ x for x in self if fnmatchcase(x, wildcard) ]
+
+    # multiple wildcard match, separate wildcards with | symbol in the argument
+    def multi_wildcard_match(self, wildcards):
+        if self.hasAttribute('nkd_fnmatchcase'):
+            fnmatchcase = self.nkd_fnmatchcase
+        else:
+            from fnmatch import fnmatchcase
+            self.nkd_fnmatchcase = fnmatchcase
+        wc_list = wildcards.split('|')
+        return_list = []
+        for wc in wc_list:
+            temp_list = [ x for x in self if fnmatchcase(x, wc) ]
+            for result in temp_list:
+                return_list.append(result)
+        return return_list
 
     #------------------------------------------------------------------------------
     # Conversion Methods
@@ -256,6 +372,12 @@ class XPriorityQueue(NakedObject):
 
     # O(log n) complexity
     def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+    # push new object and return the highest priority object
+    def push_and_pop(self, the_object, priority):
+        heapq.heappush(self._queue, (-priority, self._index, the_object))
+        self._index += 1
         return heapq.heappop(self._queue)[-1]
 
 
@@ -330,6 +452,8 @@ class XFSet(frozenset):
 # [ XString class ]
 #   An inherited extension to the immutable string object that permits attributes
 #   Immutable so there is no setter method, attributes must be set in the constructor
+#   Python 2: byte string by default, can cast to normalized UTF-8 with unicode() method
+#   Python 3: unicode Py3 string by default, can normalize with unicode() method
 #------------------------------------------------------------------------------
 class XString(str):
     def __new__(cls, string_text, attributes={}):
@@ -365,10 +489,11 @@ class XString(str):
         else:
             return self.split(split_delimiter)
 
+    # split the string on one or more characters and return items in set
     def xsplit_set(self, split_delimiter):
         return set(self.xsplit(split_delimiter))
 
-    # str begins with substring - faster than str.beginswith()
+    # str begins with substring - faster than str.startswith()
     def begins(self, begin_string):
         return begin_string in self[0:len(begin_string)]
 
@@ -381,14 +506,14 @@ class XString(str):
         from fnmatch import fnmatchcase
         return fnmatchcase(self, wildcard)
 
-    # convert string to unicode
+    # convert string to normalized UTF-8 in Python 2 and 3
     def unicode(self):
-        if not isinstance(self, unicode):
-            import unicodedata
-            u_string = unicode(self)
-            return unicodedata.normalize('NFKC', u_string)
+        from sys import version_info
+        from unicodedata import normalize
+        if version_info[0] == 2:
+            return normalize('NFKD', self.decode('UTF-8'))
         else:
-            return self
+            return normalize('NFKD', self)
 
 #------------------------------------------------------------------------------
 # [ XTuple class ]
@@ -403,12 +528,11 @@ class XTuple(tuple):
         return tup_obj
 
 
-
-
 if __name__ == '__main__':
     # pass
     # nl = XList(['a', 'b', 'c'], {"version":"1.0.1", "test":"code"})
-    # nl << ['d', 'e', 'f']
+    # nl = nl + ['d', 'e', 'f'] + ['g', 'h', 'i']
+    # print(nl)
     # the_list = list(range(5000))
     # nl = XList(the_list)
     # nq = XPriorityQueue()
@@ -419,10 +543,9 @@ if __name__ == '__main__':
     # print(nq.pop())
     # print(nq.pop())
 
-    # nl = XList(['test', 'test', 'another'], {'p': 'attribute'})
+    # nl = XList([2, 2, 2, 'another'], {'p': 'attribute'})
     # print(nl)
-    # nl = nl.remove_dupes()
-    # print(nl)
+    # print(nl.count_item(2))
     # nq = XQueue(nl, max_length=2)
     # print(nq)
 
@@ -430,11 +553,18 @@ if __name__ == '__main__':
     # xs += {'bogus', 'yep'}
     # print(xs)
 
-    xd = XDict({'test': 'yep', 'another': 'yep'}, {'a': '1', 'b': '2'})
-    ad = {'test2': 'yes', 'is':'more'}
-    xd << ad
-    print(xd.value_count('yep'))
+    # xd = XDict({'test': 0, 'another': 0}, {'a': '1', 'b': '2'})
+    # ad = {'test2': 0, 'is': 0}
+    # ld = {'last': 0}
+    # xd = xd + ad + ld
+    # print(xd)
+    # print(xd.conditional_map_to_vals(matcher, resulter))
 
-    # xstr = XString("This is a, really long string, that contains a: few cool things.tx")
-    # print(xstr.unicode())
+    # nl = XList([ 'test.txt', 'bogus.txt', 'test.py', 'another.rb', 'est.doc', 'est.py' ])
+    # print(nl.multi_wildcard_match('*.py|*.txt|*.doc'))
+
+    # xstr = XString("Hey! Cœur It's Bengali ব য,\nand here is some more ২")
+    # ustr = xstr.unicode()
+    # print(isinstance(ustr, bytes))
+    # print(xstr)
 
