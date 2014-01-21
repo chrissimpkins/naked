ec95f350b7687f58ebc6402f1f055a3bcb041fc3
Chris <git.simpkins@gmail.com>
2014-01-21 18:57:29 -0500

    updated usage command module

--- lib/Naked/commands/usage.py
+++ lib/Naked/commands/usage.py
@@ -6,11 +6,9 @@ import Naked.settings
 class Usage:
     def __init__(self):
         self.usage = Naked.settings.usage
-        self.name = Naked.settings.app_name
 
     def print_usage(self):
-        usage_string = "Usage: " + self.name + " " + self.usage
-        print(usage_string)
+        print(self.usage)
 
 
 if __name__ == '__main__':
