2f783054fda23b8dd0368877f1cfa30af775dac6
Chris <git.simpkins@gmail.com>
2014-01-21 19:00:14 -0500

    new build command module

---
+++ lib/Naked/commands/build.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+#------------------------------------------------------------------------------
+# [ compile_c_code function ] (--none--)
+#  compile C files in the lib/Naked/toolshed/c directory
+#------------------------------------------------------------------------------
+def compile_c_code(abs_dirpath):
+	from Naked.toolshed.shell import run
+	from os import chdir
+
+	chdir(abs_dirpath)
+	run("python setup.py build_ext --inplace")
+
+
+if __name__ == '__main__':
+	pass
