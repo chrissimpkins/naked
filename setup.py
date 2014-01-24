381a6ad93083cad4798a7cc7162c9daba044d2c6
Chris <git.simpkins@gmail.com>
2014-01-24 18:56:09 -0500

    modified to include version read function defined in file

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
 import os
+import re
 from setuptools import setup, find_packages
-from Naked.commands.version import Version
 
 
-def file_read(fname):
+def docs_read(fname):
     return open(os.path.join(os.path.dirname(__file__), 'docs', fname)).read()
+
+def version_read():
+    settings_file = open(os.path.join(os.path.dirname(__file__), 'lib', 'Naked', 'settings.py')).read()
+    major_regex = """major_version\s*?=\s*?["']{1}(\d+)["']{1}"""
+    minor_regex = """minor_version\s*?=\s*?["']{1}(\d+)["']{1}"""
+    patch_regex = """patch_version\s*?=\s*?["']{1}(\d+)["']{1}"""
+    major_match = re.search(major_regex, settings_file)
+    minor_match = re.search(minor_regex, settings_file)
+    patch_match = re.search(patch_regex, settings_file)
+    major_version = major_match.group(1)
+    minor_version = minor_match.group(1)
+    patch_version = patch_match.group(1)
+    if len(major_version) == 0:
+        major_version = 0
+    if len(minor_version) == 0:
+        minor_version = 0
+    if len(patch_version) == 0:
+        patch_version = 0
+    return major_version + "." + minor_version + "." + patch_version
+
 
 setup(
     name='Naked',
-    version=Version().get_version(),
+    version=version_read(),
     description='A command line application framework',
-    long_description=(file_read('README.rst')),
+    long_description=(docs_read('README.rst')),
     url='http://github.com/chrissimpkins/naked/',
     license='MIT',
     author='Christopher Simpkins',
