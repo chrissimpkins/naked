47ede35f45e5efa879d6ff372aa4f7e0291d52de
Chris <git.simpkins@gmail.com>
2014-01-25 12:27:55 -0500

    new network module tests

--- tests/test_NETWORK.py
+++ tests/test_NETWORK.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import os
 import unittest
 from Naked.toolshed.network import HTTP
 from Naked.toolshed.state import StateObject
+from Naked.toolshed.system import file_exists
+from Naked.toolshed.file import FileReader
 state = StateObject()
 
 class NakedCommandParseTest(unittest.TestCase):
@@ -49,6 +52,68 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."""
 		if state.py2:
 			self.http_string = unicode(self.http_string)
 		self.assertEqual(http_text.strip(), self.http_string.strip())
+
+	def test_http_get_binary(self):
+		"""Test HTTP get request with a binary file"""
+		http = HTTP("https://github.com/chrissimpkins/six-four/tarball/master")
+		http.get_bin_write_file(os.path.join('testfiles', 'testdir', 'test.tar.gz'))
+		self.assertTrue(file_exists(os.path.join('testfiles', 'testdir', 'test.tar.gz')))
+
+	def test_http_get_text(self):
+		"""Test HTTP get request for text data and write of text file"""
+		http = HTTP("https://raw.github.com/chrissimpkins/six-four/master/LICENSE")
+		response = http.get_txt_write_file(os.path.join('testfiles', 'testdir', 'test.txt'))
+		fr = FileReader(os.path.join('testfiles', 'testdir', 'test.txt'))
+		the_dl_text = fr.read()
+		self.assertEqual(the_dl_text.strip(), self.http_string.strip()) #test that the file write is appropriate
+		self.assertEqual(response, True) # test that the response from the method is correct (True on completed file write)
+
+	def test_http_head(self):
+		"""Test HTTP head response"""
+		http = HTTP("http://www.google.com")
+		head = http.head()
+		self.assertEqual(head['content-type'], "text/html; charset=ISO-8859-1")
+
+	def test_http_response_none(self):
+		"""Test that the HTTP response is None before the run of a HTTP method"""
+		http = HTTP('http://google.com')
+		self.assertEqual(None, http.res)
+
+	def test_http_get_response_change(self):
+		"""Confirm that the response object is changed from None after a HTTP GET method"""
+		http = HTTP("http://www.google.com")
+		http.get()
+		self.assertNotEqual(None, http.res)
+
+	def test_http_post_reponse_change(self):
+		"""Confirm that the reponse object is changed from None after a HTTP POST method"""
+		http = HTTP('http://www.google.com')
+		http.post()
+		self.assertNotEqual(None, http.res)
+
+	def test_http_get_response_check(self):
+		"""Confirm that the response object contains appropriate returned data"""
+		http = HTTP('http://www.google.com')
+		http.get()
+		self.assertEqual(http.res.status_code, 200) # test the response object attribute
+		response = http.response()
+		self.assertEqual(response.status_code, 200) # test the returned object from the response() method
+
+	def test_http_get_status_check_true(self):
+		"""Confirm the truth check for status response code OK (=200) is True when should be true"""
+		http = HTTP('http://www.google.com')
+		self.assertEqual(http.get_status_ok(), True)
+		self.assertEqual(http.res.status_code, 200) #confirm that the response object is set after the get_status_ok() call
+
+
+	def test_http_get_status_check_false(self):
+		"""Confirm the truth check for status response code OK is False for non-existent site"""
+		http = HTTP('http://www.abogussitename.com')
+		self.assertEqual(http.get_status_ok(), False)
+		self.assertEqual(http.res, None) # confirm that the response object is None when site does not exist
+
+
+
 
 
 
