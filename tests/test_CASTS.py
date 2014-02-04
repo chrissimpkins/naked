324d363adfd1348ed5677f1a01d352de795f90e1
Chris <git.simpkins@gmail.com>
2014-02-04 16:56:03 -0500

    updated casts module tests

--- tests/test_CASTS.py
+++ tests/test_CASTS.py
@@ -3,8 +3,8 @@
 
 import unittest
 import Naked.commandline
-from Naked.toolshed.types import XString, XDict, XList, XSet, XFSet, XTuple
-from Naked.toolshed.casts import xstr, xd, xl, xq, xset, xfset, xt
+from Naked.toolshed.types import XString, XDict, XList, XSet, XFSet, XTuple, NakedObject
+from Naked.toolshed.casts import xstr, xd, xl, xq, xset, xfset, xt, nobj
 
 class NakedCastsTest(unittest.TestCase):
 
@@ -22,12 +22,14 @@ class NakedCastsTest(unittest.TestCase):
         self.fsettype = type(self.frozenset)
         self.tupletype = type(self.testtuple)
 
+        self.nobj = NakedObject(self.testdict)
         self.xstring = XString(self.teststring, self.testdict)
         self.xdict = XDict(self.testdict, self.testdict)
         self.xlist = XList(self.testlist, self.testdict)
         self.xsets = XSet(self.testset, self.testdict)
         self.xfsets = XFSet(self.testset, self.testdict)
         self.xtuple = XTuple(self.testtuple, self.testdict)
+        self.nobjtype = type(self.nobj)
         self.xstringtype = type(self.xstring)
         self.xdicttype = type(self.xdict)
         self.xlisttype = type(self.xlist)
@@ -35,12 +37,13 @@ class NakedCastsTest(unittest.TestCase):
         self.xfsettype = type(self.xfsets)
         self.xtupletype = type(self.xtuple)
 
-        self.test_xstring = xstr(self.teststring)
-        self.test_xdict = xd(self.testdict)
-        self.test_xlist = xl(self.testlist)
-        self.test_xset = xset(self.testset)
-        self.test_xfset = xfset(self.frozenset)
-        self.test_xtuple = xt(self.testtuple)
+        self.test_nobj = nobj(self.testdict)
+        self.test_xstring = xstr(self.teststring, self.testdict)
+        self.test_xdict = xd(self.testdict, self.testdict)
+        self.test_xlist = xl(self.testlist, self.testdict)
+        self.test_xset = xset(self.testset, self.testdict)
+        self.test_xfset = xfset(self.frozenset, self.testdict)
+        self.test_xtuple = xt(self.testtuple, self.testdict)
 
     def tearDown(self):
         pass
@@ -71,6 +74,9 @@ class NakedCastsTest(unittest.TestCase):
     # Naked type extension assertions
     #------------------------------------------------------------------------------
 
+    def test_naked_object_naked_type(self):
+        self.assertEqual(self.nobjtype, type(self.nobj))
+
     def test_xstring_naked_type(self):
         self.assertEqual(self.xstringtype, type(self.xstring))
 
@@ -92,6 +98,9 @@ class NakedCastsTest(unittest.TestCase):
     #------------------------------------------------------------------------------
     # Confirm that Python type is not equal to the Naked type
     #------------------------------------------------------------------------------
+
+    def test_nobj_not_equal_dict_type(self):
+        self.assertNotEqual(self.nobjtype, self.dicttype)
 
     def test_xstring_not_equal_string_type(self):
         self.assertNotEqual(self.xstringtype, self.stringtype)
@@ -115,6 +124,9 @@ class NakedCastsTest(unittest.TestCase):
     # Confirm that casts create objects that are of Naked types
     #------------------------------------------------------------------------------
 
+    def test_naked_object_from_dict_type(self):
+        self.assertEqual(self.nobjtype, type(self.test_nobj))
+
     def test_xstring_from_string_type(self):
         self.assertEqual(self.xstringtype, type(self.test_xstring))
 
@@ -137,6 +149,9 @@ class NakedCastsTest(unittest.TestCase):
     # And confirm that casted objects are no longer of original Python types
     #------------------------------------------------------------------------------
 
+    def test_naked_obj_not_dict_type(self):
+        self.assertNotEqual(self.dicttype, type(self.test_nobj))
+
     def test_xstring_not_string_type(self):
         self.assertNotEqual(self.stringtype, type(self.test_xstring))
 
@@ -156,8 +171,29 @@ class NakedCastsTest(unittest.TestCase):
         self.assertNotEqual(self.tupletype, type(self.test_xtuple))
 
     #------------------------------------------------------------------------------
-    # TODO: confirm that the cast appropriately assigned attributes
+    # Confirm that the cast appropriately assigned attributes
     #------------------------------------------------------------------------------
+
+    def test_naked_obj_attr(self):
+        self.assertEqual('val1', self.test_nobj.key1)
+
+    def test_xstring_obj_attr(self):
+        self.assertEqual('val1', self.test_xstring.key1)
+
+    def test_xdict_obj_attr(self):
+        self.assertEqual('val1', self.test_xdict.key1)
+
+    def test_xlist_obj_attr(self):
+        self.assertEqual('val1', self.test_xlist.key1)
+
+    def test_xset_obj_attr(self):
+        self.assertEqual('val1', self.test_xset.key1)
+
+    def test_xfset_obj_attr(self):
+        self.assertEqual('val1', self.test_xfset.key1)
+
+    def test_xtuple_obj_attr(self):
+        self.assertEqual('val1', self.test_xtuple.key1)
 
 
 
