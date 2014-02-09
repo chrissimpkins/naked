5bf0ca0f3978d268160297efc90c23c25fe49cf8
Chris <git.simpkins@gmail.com>
2014-02-09 17:46:56 -0500

    new scratch

--- lib/Naked/scratchpad.py
+++ lib/Naked/scratchpad.py
@@ -11,14 +11,20 @@ def print_scratch(func):
 
 
 def run_scratchpad():
-    from Naked.toolshed.file import FileReader
-    from Naked.toolshed.types import XString, XUnicode
+    # from Naked.toolshed.file import FileReader
+    # from Naked.toolshed.types import XString, XUnicode
 
-    r = FileReader('/Users/ces/Desktop/code/naked/tests/testfiles/unicode.txt')
-    test1 = r.read_utf8()
-    test2 = XUnicode(test1, {'a': 'b'})
-    test3 = "Hey! It's Bengali ব য,and here is some more ২"
-    print(unicode(test2))
+    # r = FileReader('/Users/ces/Desktop/code/naked/tests/testfiles/unicode.txt')
+    # test1 = r.read_utf8()
+    # test2 = XUnicode(test1, {'a': 'b'})
+    # test3 = "Hey! It's Bengali ব য,and here is some more ২"
+    # print(unicode(test2))
+
+    from Naked.toolshed.network import HTTP
+
+    http = HTTP("https://github.com/chrissimpkins/six-four/tarball/master")
+    bin_data = http.get_data()
+    print(bin_data)
 
 
 
