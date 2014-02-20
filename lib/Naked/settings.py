966bc6bc76676e773d18abde2f3226b367f254d3
Chris <git.simpkins@gmail.com>
2014-02-19 22:10:01 -0500

    version bump

--- lib/Naked/settings.py
+++ lib/Naked/settings.py
@@ -11,7 +11,7 @@ app_name = "naked"
 #------------------------------------------------------------------------------
 major_version = "0"
 minor_version = "1"
-patch_version = "25"
+patch_version = "26"
 
 #------------------------------------------------------------------------------
 # Debug Flag (switch to False for production release code)
